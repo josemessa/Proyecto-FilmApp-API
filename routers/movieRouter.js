76c04ddb09ffec768d6f17927ad9f0ab0c158019
josemesa <josemesamarin@gmail.com>
2024-05-17 11:43:27 +0200

    added Latest movies swagger doc

--- routers/movieRouter.js
+++ routers/movieRouter.js
@@ -13,7 +13,7 @@ const { verifyToken, verifyAdmin } = require("../middlewares/auth");
  * @swagger
  * /movies:
  *   post:
- *     summary: Add a new movie
+ *     summary: Añadir una nueva pelicula
  *     requestBody:
  *       required: true
  *       content:
@@ -23,13 +23,13 @@ const { verifyToken, verifyAdmin } = require("../middlewares/auth");
  *             properties:
  *               title:
  *                 type: string
- *                 example: "Inception"
+ *                 example: "Matrix"
  *               description:
  *                 type: string
- *                 example: "A mind-bending thriller"
+ *                 example: "Hasta donde llegarias para conocer la verdad?"
  *               posterUrl:
  *                 type: string
- *                 example: "http://example.com/inception.jpg"
+ *                 example: "http://example.com/matrix.jpg"
  *               adult:
  *                 type: boolean
  *                 example: false
@@ -45,10 +45,10 @@ router.post("/", verifyToken, verifyAdmin, addMovie);
  * @swagger
  * /movies:
  *   get:
- *     summary: Get all movies
+ *     summary: Obtener todas las peliculas
  *     responses:
  *       200:
- *         description: Returns all movies
+ *         description: Todas las peliculas han sido obtenidas
  *
  *       204:
  *         description: No movies found
@@ -58,7 +58,41 @@ router.post("/", verifyToken, verifyAdmin, addMovie);
  *
  */
 router.get("/", getAllMovies);
+/**
+ * @swagger
+ * /latest-movies:
+ *   get:
+ *     summary: Obtener las ultimas peliculas añadidas
+ *     responses:
+ *       200:
+ *         description: Ultimas peliculas obtenidas
+ *       204:
+ *         description: No latest movies found
+ *       400:
+ *         description: Error getting latest movies
+ */
+
 router.get("/latest", getLatestMovies);
+/**
+ * @swagger
+ * /movies/{id}:
+ *   get:
+ *     summary: Obtener una pelicula por ID
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: ID de la pelicula que buscamos
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Pelicula obtenida correctamente
+ *       204:
+ *         description: No movie found with that ID
+ *       400:
+ *         description: Error getting movie by ID
+ */
 router.get("/:id", getMovieById);
 router.patch("/:id", verifyToken, verifyAdmin, updateMovie);
 router.delete("/:id/deletemovie", verifyToken, verifyAdmin, deleteMovie);
